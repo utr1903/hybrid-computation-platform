dd92acc0c2bd562d52fc5f44b460cd7586eb8c9c
uturkarslan <ugurturkarslan@gmail.com>
2024-08-03 13:46:38 +0200

    Add createorganization topic

--- apps/helm/kafka/deploy.sh
+++ apps/helm/kafka/deploy.sh
@@ -21,7 +21,9 @@ helm upgrade ${kafkaName} \
   --namespace=${kafkaNamespace} \
   --set listeners.client.protocol=PLAINTEXT \
   --set provisioning.enabled=true \
-  --set provisioning.topics[0].name="jobrequest" \
+  --set provisioning.topics[0].name="createorganization" \
   --set provisioning.topics[0].partitions=3 \
+  --set provisioning.topics[1].name="jobrequest" \
+  --set provisioning.topics[1].partitions=3 \
   --version "26.6.2" \
   "bitnami/kafka"
