6a9c6b7e0c39fa85e121c4700d9a1809d06fa13e
uturkarslan <ugurturkarslan@gmail.com>
2024-08-04 00:05:25 +0200

    Add updatejob topic

--- apps/helm/kafka/deploy.sh
+++ apps/helm/kafka/deploy.sh
@@ -27,5 +27,7 @@ helm upgrade ${kafkaName} \
   --set provisioning.topics[1].partitions=3 \
   --set provisioning.topics[2].name="createjob" \
   --set provisioning.topics[2].partitions=3 \
+  --set provisioning.topics[3].name="updatejob" \
+  --set provisioning.topics[3].partitions=3 \
   --version "26.6.2" \
   "bitnami/kafka"
